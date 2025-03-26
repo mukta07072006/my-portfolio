b2c12ce794c3ba366e52d24bed9129a54dff4f83
mukta07072006 <146613715+mukta07072006@users.noreply.github.com>
2025-03-26 21:24:26 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-   const nextConfig = {
+
+const nextConfig = {
      reactStrictMode: true,
-     swcMinify: false, // Disabled for stability
-     output: 'standalone' // Optimized for Vercel
+     swcMinify: true // Required for App Router
    };
-   module.exports = nextConfig;
