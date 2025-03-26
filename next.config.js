45565b6ea418a193a3729b2885552ac505f61cee
mukta07072006 <146613715+mukta07072006@users.noreply.github.com>
2025-03-26 20:43:53 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+   const nextConfig = {
+     reactStrictMode: true,
+     // Disable SWC if errors persist (uncomment below)
+     // swcMinify: false,
+   };
+   module.exports = nextConfig;
