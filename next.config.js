4c5fdd3818e155b367827fb24c43247c38b5f9bc
mukta07072006 <146613715+mukta07072006@users.noreply.github.com>
2025-03-26 20:37:51 +0600

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Add other options only if needed
-};
-
-module.exports = nextConfig;
