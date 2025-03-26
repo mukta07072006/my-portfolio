5147c7afaa5f04f3bb6ed072620ceb06dcd7ba2b
mukta07072006 <146613715+mukta07072006@users.noreply.github.com>
2025-03-26 22:17:03 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+   const nextConfig = {
+     reactStrictMode: true,
+     output: 'standalone' // ← Critical for Vercel
+   };
+   module.exports = nextConfig;
