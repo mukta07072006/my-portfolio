4a61d63baf3537e21b4b0d44524aec641a7d92c0
mukta07072006 <146613715+mukta07072006@users.noreply.github.com>
2025-03-26 20:19:26 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-module.exports = {
-  swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false
-  }
-}
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  }
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add other options if needed (e.g., images, redirects)
+};
 
+module.exports = nextConfig;
